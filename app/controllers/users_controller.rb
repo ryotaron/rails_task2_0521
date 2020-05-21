a0c9144d25df37979420749cfa4b7bd903720741
nakaryo <nakaryo@nakaryonoMacBook-ea.local>
2020-05-21 12:17:55 +0900

    一覧画面まで実装済み

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,16 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
 
-   def create
+   def createf
     User.create(user_params)
    end
+
 
      private
      def user_params
