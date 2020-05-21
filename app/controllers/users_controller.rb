a0fd0a41ed7f3f7d40860a00c0906186b6fce77c
nakaryo <nakaryo@nakaryonoMacBook-ea.local>
2020-05-21 13:12:40 +0900

    編集操作の画面作成まで実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,9 @@ class UsersController < ApplicationController
     User.create(user_params)
    end
 
+   def edit
+     @user = User.find(params[:id]) 
+   end
    def destroy
      user = User.find(params[:id])
      user.destroy
